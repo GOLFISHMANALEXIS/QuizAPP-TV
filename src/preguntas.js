c1a7777b272e57e5be2e1942182191cd64c7daf8
GOLFISHMANALEXIS <dhhdkalalo@gmail.com>
2022-06-10 00:46:48 -0500

    uanisdjasid

---
+++ src/preguntas.js
@@ -0,0 +1,49 @@
+const preguntas = [
+  {
+    titulo: "¿Que dia empezamos a hablar x mensaje?",
+    opciones: [
+      { textoRespuesta: "2 de Abril", isCorrect: true },
+      { textoRespuesta: "1 de Abril", isCorrect: false },
+      { textoRespuesta: "5 de abril", isCorrect: false },
+      { textoRespuesta: "3 de abril", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "¿Que dia es nuestro aniversario?",
+    opciones: [
+      { textoRespuesta: "15 de abril", isCorrect: false },
+      { textoRespuesta: "30 de abril", isCorrect: false },
+      { textoRespuesta: "17 de mayo", isCorrect: true },
+      { textoRespuesta: "20 de noviembre", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "¿Cuantas Apps te he hecho?",
+    opciones: [
+      { textoRespuesta: "+ de 10", isCorrect: true },
+      { textoRespuesta: "- de 10", isCorrect: false },
+      { textoRespuesta: "20", isCorrect: false },
+      { textoRespuesta: "No sabes", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "¿Cual te dedicaria primero?",
+    opciones: [
+      { textoRespuesta: "Cama y mesa", isCorrect: true },
+      { textoRespuesta: "Aprovechate de mi", isCorrect: false },
+      { textoRespuesta: "Enamorado tuyo", isCorrect: false },
+      { textoRespuesta: "Remolino ", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "¿Cuatos poemas te he dedicado?",
+    opciones: [
+      { textoRespuesta: "15", isCorrect: false },
+      { textoRespuesta: "8", isCorrect: false },
+      { textoRespuesta: "10", isCorrect: false },
+      { textoRespuesta: "13 ", isCorrect: true },
+    ],
+  },
+];
+
+export default preguntas;
